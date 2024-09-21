ef9eb322102e12010bbb39792b1b1117445c65db
chp9-u <63417401+chp9-u@users.noreply.github.com>
2024-09-20 23:38:04 -0400

    Reduced the size of the package name under the required 28 chars

--- .yamato/scripts/package.ps1
+++ .yamato/scripts/package.ps1
@@ -6,5 +6,5 @@ cp LICENSE out/LICENSE.md
 cp "./tools/vcpkg/installed/${env:OPENTELEMETRY_CPP_LIBTYPE}/lib/*.*" $extra -verbose
 
 push-location "out"
-Compress-Archive -Path ./* -DestinationPath "../opentelemetry-cpp-$($env:OPENTELEMETRY_CPP_LIBTYPE -replace "-static-md").zip"
+Compress-Archive -Path ./* -DestinationPath "../$($args[0])"
 Pop-Location # out
--- .yamato/scripts/package.sh
+++ .yamato/scripts/package.sh
@@ -6,5 +6,5 @@ cp LICENSE out/LICENSE.md
 cp tools/vcpkg/installed/$OPENTELEMETRY_CPP_LIBTYPE/lib/*.* out/lib/extra/
 
 pushd out
-zip -r "../opentelemetry-cpp-$OPENTELEMETRY_CPP_LIBTYPE.zip" *
+zip -r "../$1" *
 popd # out
