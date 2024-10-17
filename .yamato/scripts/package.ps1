4b25dd4df3c878e0e9d15829bebf82c6d2e1df47
chp9-u <63417401+chp9-u@users.noreply.github.com>
2024-10-17 09:59:31 -0400

    removed some complexity of the package

--- .yamato/scripts/package.ps1
+++ .yamato/scripts/package.ps1
@@ -2,15 +2,15 @@ $ErrorActionPreference = "Stop"
 $ProgressPreference = "SilentlyContinue"
 
 mkdir "package/include" -force | Out-Null
-mkdir "package/lib/${env:OPENTELEMETRY_CPP_LIBTYPE}/release/extra" -force | Out-Null
-mkdir "package/lib/${env:OPENTELEMETRY_CPP_LIBTYPE}/debug/extra" -force | Out-Null
+mkdir "package/lib/release/extra" -force | Out-Null
+mkdir "package/lib/debug/extra" -force | Out-Null
 
 cp LICENSE package/LICENSE.md
 cp -R "out/Release/include/." "package/include/"
-cp -R "out/Release/lib/." "package/lib/${env:OPENTELEMETRY_CPP_LIBTYPE}/release/"
-cp -R "tools/vcpkg/installed/${env:OPENTELEMETRY_CPP_LIBTYPE}/lib/." "package/lib/${env:OPENTELEMETRY_CPP_LIBTYPE}/release/extra/"
-cp -R "out/Debug/lib/." "package/lib/${env:OPENTELEMETRY_CPP_LIBTYPE}/debug/"
-cp -R "tools/vcpkg/installed/${env:OPENTELEMETRY_CPP_LIBTYPE}/debug/lib/." "package/lib/${env:OPENTELEMETRY_CPP_LIBTYPE}/debug/extra/"
+cp -R "out/Release/lib/." "package/lib/release/"
+cp -R "tools/vcpkg/installed/${env:OPENTELEMETRY_CPP_LIBTYPE}/lib/." "package/lib/release/extra/"
+cp -R "out/Debug/lib/." "package/lib/debug/"
+cp -R "tools/vcpkg/installed/${env:OPENTELEMETRY_CPP_LIBTYPE}/debug/lib/." "package/lib/debug/extra/"
 
 push-location "out"
 Compress-Archive -Path ./* -DestinationPath "../$($args[0])"
--- .yamato/scripts/package.sh
+++ .yamato/scripts/package.sh
@@ -2,15 +2,15 @@
 set -euxo pipefail
 
 mkdir -p "package/include"
-mkdir -p "package/lib/$OPENTELEMETRY_CPP_LIBTYPE/release/extra"
-mkdir -p "package/lib/$OPENTELEMETRY_CPP_LIBTYPE/debug/extra"
+mkdir -p "package/lib/release/extra"
+mkdir -p "package/lib/debug/extra"
 
 cp LICENSE package/LICENSE.md
 cp -R "out/Release/include/." "package/include/"
-cp -R "out/Release/lib/." "package/lib/$OPENTELEMETRY_CPP_LIBTYPE/release/"
-cp -R "tools/vcpkg/installed/$OPENTELEMETRY_CPP_LIBTYPE/lib/." "package/lib/$OPENTELEMETRY_CPP_LIBTYPE/release/extra/"
-cp -R "out/Debug/lib/." "package/lib/$OPENTELEMETRY_CPP_LIBTYPE/debug/"
-cp -R "tools/vcpkg/installed/$OPENTELEMETRY_CPP_LIBTYPE/debug/lib/." "package/lib/$OPENTELEMETRY_CPP_LIBTYPE/debug/extra/"
+cp -R "out/Release/lib/." "package/lib/release/"
+cp -R "tools/vcpkg/installed/$OPENTELEMETRY_CPP_LIBTYPE/lib/." "package/lib/release/extra/"
+cp -R "out/Debug/lib/." "package/lib/debug/"
+cp -R "tools/vcpkg/installed/$OPENTELEMETRY_CPP_LIBTYPE/debug/lib/." "package/lib/debug/extra/"
 
 pushd package
 zip -r "../$1" *
