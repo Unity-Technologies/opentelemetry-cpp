af0866ee8867212025b9f1d02dd331db15711411
chp9-u <63417401+chp9-u@users.noreply.github.com>
2024-10-17 10:08:35 -0400

    Fixed Windows packaging script

--- .yamato/scripts/package.ps1
+++ .yamato/scripts/package.ps1
@@ -6,11 +6,11 @@ mkdir "package/lib/release/extra" -force | Out-Null
 mkdir "package/lib/debug/extra" -force | Out-Null
 
 cp LICENSE package/LICENSE.md
-cp -R "out/Release/include/." "package/include/"
-cp -R "out/Release/lib/." "package/lib/release/"
-cp -R "tools/vcpkg/installed/${env:OPENTELEMETRY_CPP_LIBTYPE}/lib/." "package/lib/release/extra/"
-cp -R "out/Debug/lib/." "package/lib/debug/"
-cp -R "tools/vcpkg/installed/${env:OPENTELEMETRY_CPP_LIBTYPE}/debug/lib/." "package/lib/debug/extra/"
+cp -R "out/Release/include/*" "package/include/"
+cp -R "out/Release/lib/*" "package/lib/release/"
+cp -R "tools/vcpkg/installed/${env:OPENTELEMETRY_CPP_LIBTYPE}/lib/*" "package/lib/release/extra/"
+cp -R "out/Debug/lib/*" "package/lib/debug/"
+cp -R "tools/vcpkg/installed/${env:OPENTELEMETRY_CPP_LIBTYPE}/debug/lib/*" "package/lib/debug/extra/"
 
 push-location "out"
 Compress-Archive -Path ./* -DestinationPath "../$($args[0])"
